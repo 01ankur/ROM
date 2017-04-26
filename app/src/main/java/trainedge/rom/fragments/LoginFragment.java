3d6743e9d82989911e692727cc352bcb158c61ab
01ankur <attitudeankursingh@gmail.com>
2017-04-26 21:35:57 +0530

    updated login ui\

--- app/src/main/java/trainedge/rom/fragments/LoginFragment.java
+++ app/src/main/java/trainedge/rom/fragments/LoginFragment.java
@@ -3,8 +3,10 @@ package trainedge.rom.fragments;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,11 +14,29 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.DeviceLoginButton;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+
+import trainedge.rom.Login;
 import trainedge.rom.R;
 import trainedge.rom.RegisterActivity;
 import trainedge.rom.UserListingActivity;
 import trainedge.rom.core.login.LoginContract;
 import trainedge.rom.core.login.LoginPresenter;
+
+import static trainedge.rom.HomeActivity.TAG;
 
 public class LoginFragment extends Fragment implements View.OnClickListener, LoginContract.View {
     private LoginPresenter mLoginPresenter;
@@ -25,6 +45,10 @@ public class LoginFragment extends Fragment implements View.OnClickListener, Log
     private Button mBtnLogin, mBtnRegister;
 
     private ProgressDialog mProgressDialog;
+    private SignInButton mBtnGoogle;
+    private LoginButton mBtnFacebook;
+    private CallbackManager mCallbackManager;
+    private FirebaseAuth mAuth;
 
     public static LoginFragment newInstance() {
         Bundle args = new Bundle();
@@ -33,10 +57,18 @@ public class LoginFragment extends Fragment implements View.OnClickListener, Log
         return fragment;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View fragmentView = inflater.inflate(R.layout.fragment_login, container, false);
+
         bindViews(fragmentView);
         return fragmentView;
     }
@@ -45,6 +77,8 @@ public class LoginFragment extends Fragment implements View.OnClickListener, Log
         mETxtEmail = (EditText) view.findViewById(R.id.edit_text_email_id);
         mETxtPassword = (EditText) view.findViewById(R.id.edit_text_password);
         mBtnLogin = (Button) view.findViewById(R.id.button_login);
+        mBtnGoogle = (SignInButton) view.findViewById(R.id.btnGoogle);
+        mBtnFacebook = (LoginButton) view.findViewById(R.id.btnFbLogin);
         mBtnRegister = (Button) view.findViewById(R.id.button_register);
     }
 
@@ -64,13 +98,34 @@ public class LoginFragment extends Fragment implements View.OnClickListener, Log
 
         mBtnLogin.setOnClickListener(this);
         mBtnRegister.setOnClickListener(this);
+        mBtnGoogle.setOnClickListener(this);
 
         setDummyCredentials();
+        mCallbackManager = CallbackManager.Factory.create();
+        mBtnFacebook.setReadPermissions("email", "public_profile");
+        mBtnFacebook.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getActivity(), "Cancelled Facebook login", Toast.LENGTH_SHORT).show();
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(getActivity(), "error :" + error.getMessage(), Toast.LENGTH_SHORT).show();
+                // ...
+            }
+        });
     }
 
     private void setDummyCredentials() {
-        mETxtEmail.setText("test@test.com");
-        mETxtPassword.setText("123456");
+
     }
 
     @Override
@@ -111,5 +166,25 @@ public class LoginFragment extends Fragment implements View.OnClickListener, Log
     public void onLoginFailure(String message) {
         mProgressDialog.dismiss();
         Toast.makeText(getActivity(), "Error: " + message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(getActivity(), "Authentication failed " + task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
